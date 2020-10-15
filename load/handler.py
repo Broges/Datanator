af001098c87d0e9ee099eea84fcb9cf77c97b44e
Fahid Talukdar <fahid.t.work@gmail.com>
2020-10-15 14:42:28 +0100

    load 13

--- load/handler.py
+++ load/handler.py
@@ -8,19 +8,18 @@ def lambda_handler(event,context):
     
     the_message = recieve_sqs_message(event)
     bigData = the_message[2:-2]
-    
+    listOfBasketObjects = []
+    listOfTransactionObjects=[]
     all_data = bigData.split('], [')
     for item in all_data:
         attributes = item.split(',')
         if len(attributes) == 4:
-            listOfBasketObjects = []
             basketObject = Basket(attributes[0],attributes[1],attributes[2],attributes[3])
             listOfBasketObjects.append(basketObject)
             if item == all_data[-1]:
                 redshiftHandler.importDataToBasketTable(listOfBasketObjects)
     
         elif len(attributes) == 5:
-            listOfTransactionObjects=[]
             transactionObject = Transaction(attributes[0],attributes[1],attributes[2],attributes[3],attributes[4])
             listOfTransactionObjects.append(transactionObject)
             if item == all_data[-1]:
