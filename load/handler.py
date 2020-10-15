349adc11358cf599a6e51e3a9c897a22cb542546
Fahid Talukdar <fahid.t.work@gmail.com>
2020-10-15 14:27:03 +0100

    load 12

--- load/handler.py
+++ load/handler.py
@@ -4,24 +4,25 @@ from core.basket import Basket
 from core.transaction import Transaction
 
 def lambda_handler(event,context):
-
+    redshiftHandler.redshiftConnector()
+    
     the_message = recieve_sqs_message(event)
     bigData = the_message[2:-2]
     
     all_data = bigData.split('], [')
-    for item in all_data: #has the 4 thigns 
+    for item in all_data:
         attributes = item.split(',')
         if len(attributes) == 4:
             listOfBasketObjects = []
             basketObject = Basket(attributes[0],attributes[1],attributes[2],attributes[3])
-            listOfBasketObjects.append(basketObject)            
+            listOfBasketObjects.append(basketObject)
+            if item == all_data[-1]:
+                redshiftHandler.importDataToBasketTable(listOfBasketObjects)
+    
         elif len(attributes) == 5:
             listOfTransactionObjects=[]
             transactionObject = Transaction(attributes[0],attributes[1],attributes[2],attributes[3],attributes[4])
             listOfTransactionObjects.append(transactionObject)
-    
-    redshiftHandler.redshiftConnector()
-    
-    redshiftHandler.importDataToTransactionTable(listOfTransactionObjects)
-    redshiftHandler.importDataToBasketTable(listOfBasketObjects)
+            if item == all_data[-1]:
+                redshiftHandler.importDataToTransactionTable(listOfTransactionObjects)
     redshiftHandler.closeRedshiftConnection()
