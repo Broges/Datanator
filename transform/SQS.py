7f601f84d946aa4dd2af701d0dbbecf013a347b7
MichaelFickling <michaellukefickling@gmail.com>
2020-10-19 15:07:49 +0100

    transform2

--- transform/SQS.py
+++ transform/SQS.py
@@ -5,10 +5,22 @@ import sys
 def send_sqs_message(queue_name, msg_body):
     sqs_client = boto3.client('sqs')
     # print(json.dumps(msg_body))
-    if sys.getsizeof(json.dumps(msg_body)) < 256000:
+
+    lst = []
+    lst.append(msg_body)
+    for sub_list in lst:
+        while sys.getsizeof(json.dumps(sub_list)) > 250000:
+            x = len(sub_list) // 2
+            sub1, sub2 = sub_list[0:x], sub_list[x:]
+            temp = lst.index(sub_list)
+            lst[temp] = sub1
+            lst.insert(temp + 1, sub2)
+            sub_list = sub1
+
+    for sub_list in lst:
+        print(sys.getsizeof(json.dumps(sub_list)))
         msg = sqs_client.send_message(QueueUrl="https://sqs.eu-west-1.amazonaws.com/579154747729/Team2-transform-to-load", MessageBody=json.dumps(msg_body))
-    else:
-        print(f"{msg_body[0][0]} was to big")
+    
     return
 
 def recieve_sqs_message(event):
