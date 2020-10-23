0db87bd321e21ecce9e494d5dcd9d25da76a0ad0
Fahid Talukdar <fahid.t.work@gmail.com>
2020-10-23 14:56:29 +0100

    push8

--- extract/sqs_functions/queue.py
+++ extract/sqs_functions/queue.py
@@ -4,6 +4,6 @@ import boto3
 def send_sqs_data(data, file_key):
     # sends the list of lists to the queue in the form of a string
     sqs_client = boto3.client('sqs')
-    sqs_client.send_message(QueueUrl="https://sqs.eu-west-1.amazonaws.com/579154747729/Team2-extract-to-tranform", MessageBody=json.dumps(data))
+    sqs_client.send_message(QueueUrl="https://sqs.eu-west-1.amazonaws.com/579154747729/Team2extracttotransform", MessageBody=json.dumps(data))
     print(f"{file_key} has been extracted.")
     return
