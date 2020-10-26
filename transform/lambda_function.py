871bfad6f30409308e55dab3fee656f9f0c221e5
HarryAnkcorn <harry.ankcorn@gmail.com>
2020-10-26 15:27:45 +0000

    hello worlded

--- transform/lambda_function.py
+++ transform/lambda_function.py
@@ -4,20 +4,20 @@ from transaction import clean_transaction_data
 from SQS import send_sqs_message, recieve_sqs_message
 
 def lambda_handler(event, context):
+    print("hello world")
+    # sqs_input=recieve_sqs_message(event)
+    
+    # sqs_input = sqs_input.replace("'", '"')
+    # data = json.loads(sqs_input)
+    
+    # basket_objects_list = clean_basket_data(data)
+    # transaction_objects_list = clean_transaction_data(data)
+    
+    # print(transaction_objects_list)
 
-    sqs_input=recieve_sqs_message(event)
-    
-    sqs_input = sqs_input.replace("'", '"')
-    data = json.loads(sqs_input)
-    
-    basket_objects_list = clean_basket_data(data)
-    transaction_objects_list = clean_transaction_data(data)
-    
-    print(transaction_objects_list)
-
-    print("About to send message")
-    send_sqs_message('Team2-transform-to-load', basket_objects_list)
-    send_sqs_message('Team2-transform-to-load', transaction_objects_list)
-    print("Message sent")
+    # print("About to send message")
+    # send_sqs_message('Team2-transform-to-load', basket_objects_list)
+    # send_sqs_message('Team2-transform-to-load', transaction_objects_list)
+    # print("Message sent")
     
     return
