4a578c09e46c12bb3635cda07e58e69de959cf20
Brogan Lennon <broganlennon@gmail.com>
2020-11-02 14:02:10 +0000

    file cleanup

--- team2/core/basket_class.py
+++
@@ -1,8 +0,0 @@
-class Basket:
-    def __init__(self, basket_id, transaction_id, basket_item, price):
-        self.basket_id = basket_id
-        self.transaction_id = transaction_id
-        self.basket_item = basket_item
-        self.price = price
-    def __repr__(self):
-        return (f"{self.basket_id}, {self.transaction_id}, {self.basket_item}, £{self.price}")
